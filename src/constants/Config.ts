081ef2b44bf380d548a32fd1e056e5c2485add27
Ale241302 <jorjecasanova@gmail.com>
2025-12-10 19:36:19 -0500

    Initial commit
    Generated by create-expo-app 3.5.3.

---
+++ src/constants/Config.ts
@@ -0,0 +1,11 @@
+export const Config = {
+  API_URL: 'https://muitowork.com/api-tracking',
+  LOGIN_URL: 'https://muitowork.com/api-tracking/login.php',
+  KEY_HASH: 'e474389ae582ee303d6a7d0c6307e6beb373015edfa7b67f1a02bab62367d66f',
+  IMAGES: {
+    BACKGROUND_TOP: 'https://mwt.one/images/Recurso%201paises_responsive.png',
+    BACKGROUND_BOTTOM: 'https://mwt.one/images/Recurso%203paises_responsive.png',
+    LOGO_LOGIN: 'https://mwt.one/images/Recurso%203logo_login.png',
+    APP_ICON: 'https://mwt.one/images/logomwt.png',
+  },
+};
